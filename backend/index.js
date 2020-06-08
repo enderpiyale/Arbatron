2ba5c84042bf5e2a4c63919db4fed88e342a0283
enderpiyale <enderpiyale@gmail.com>
2020-06-08 13:02:27 +0300

    Environment variables config

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,40 @@
-
 'use strict'
+require('dotenv').config();
 
-const ccxt = require ('ccxt')
-    , log = require ('ololog')
-    , asTable = require ('as-table')
+// const ccxt = require ('ccxt')
+//     , log = require ('ololog')
+//     , asTable = require ('as-table')
 
-;(async function main () {
+// ;(async function main () {
 
-    let kraken = new ccxt.kraken ({ enableRateLimit: true })
-    await kraken.loadMarkets ()
+//     let kraken = new ccxt.kraken ({ enableRateLimit: true })
+//     await kraken.loadMarkets ()
 
-    const markets = Object.values (kraken.markets).map (market => ({
-        symbol: market.symbol,
-        active: market.active,
-        taker: market.taker,
-        maker: market.maker,
-        percentage: market.percentage
-    }))
+//     const markets = Object.values (kraken.markets).map (market => ({
+//         symbol: market.symbol,
+//         active: market.active,
+//         taker: market.taker,
+//         maker: market.maker,
+//         percentage: market.percentage
+//     }))
 
-    log.bright.green.noLocate ('Markets:')
-    log.green.noLocate (asTable (markets), '\n')
+//     log.bright.green.noLocate ('Markets:')
+//     log.green.noLocate (asTable (markets), '\n')
 
-    const currencies = Object.values (kraken.currencies).map (currency => ({
-        code: currency.code,
-        active: currency.active,
-        status: currency.status,
-        fee: currency.fee
-    }))
+//     const currencies = Object.values (kraken.currencies).map (currency => ({
+//         code: currency.code,
+//         active: currency.active,
+//         status: currency.status,
+//         fee: currency.fee
+//     }))
 
-    log.bright.yellow.noLocate ('Currencies:')
-    log.yellow.noLocate (asTable (currencies))
+//     log.bright.yellow.noLocate ('Currencies:')
+//     log.yellow.noLocate (asTable (currencies))
 
-    let symbols = kraken.symbols
-    console.log(symbols)
+//     let symbols = kraken.symbols
+//     console.log(symbols)
 
-}) ()
+// }) ()
 
+const api = process.env.BINANCE_API_KEY
+console.log(api)
